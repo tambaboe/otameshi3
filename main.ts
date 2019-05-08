6ba2115d5c511512fc68d66e5ba0b7114439d151
usuratonkatu <50354701+usuratonkatu@users.noreply.github.com>
2019-05-09 08:39:39 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=70 icon="\uf05" color=#555555 block="コメント"
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
 namespace comment {
     //% blockId=show_strings block="コメント %v"
     export function noaction(text: string): void {
